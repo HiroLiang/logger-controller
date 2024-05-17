92c2d0109c18f6ae234a4e40d866d0ae9f5a17ff
HiroLiang <2301031@systex.com.tw>
2024-05-17 17:44:14 +0800

    #delete - not used

--- src/main/java/hiroliang/tools/loggercontroller/marker/MarkerConfig.java
+++ src/main/java/hiroliang/tools/loggercontroller/marker/MarkerConfig.java
@@ -3,7 +3,6 @@ package hiroliang.tools.loggercontroller.marker;
 import java.time.LocalDateTime;
 
 public interface MarkerConfig {
-    String getName();
     LocalDateTime getTimestamp();
     Integer getExpirationDays();
 }
