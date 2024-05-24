e56327ac64193d3d20ec425040bd729234935da8
HiroLiang <2301031@systex.com.tw>
2024-05-24 17:43:43 +0800

    #edit - remove - not used

--- src/main/java/hiroliang/tools/loggercontroller/marker/StandardMarkerList.java
+++ src/main/java/hiroliang/tools/loggercontroller/marker/StandardMarkerList.java
@@ -1,9 +1,7 @@
 package hiroliang.tools.loggercontroller.marker;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Marker;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
